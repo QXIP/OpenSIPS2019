a484d297788da915c861ceb58a940cdf776460e4
Bogdan Iancu <bogdan@opensips.org>
2019-03-21 20:43:18 +0200

    Added all the speakers

--- data.js
+++ data.js
@@ -1,26 +1,48 @@
 var summitData = {
    "SPEAKERS":[
       {
-         "NAME":"Bogdan-Andrei Iancu",
-         "SPEECHTITLE":"Key Notes - OpenSIPS 3.0",
-         "SPEECHABSTRACT":"OpenISPS 3.0",
-         "TEXTIMG":"http://i.imgur.com/c0q8TjB.png",
-         "TEXTTITLE":"Bogdan-Andrei Iancu",
-         "COMPANY":"OpenSIPS Project",
-         "DESC":"Bogdan is the OpenSIPS project founder with an experience of 15+ year in the SIP world. Practicing the symbioses between managing the Open Source project and building commercial products around OpenSIPS, gives the best results in producing a viable SIP Server software for the real-life needs.",
-         "TWITTER":"https://twitter.com/@bogdan_iancu",
-         "POSITION":"Founder"
+         "NAME":"Saúl Ibarra Corretgé",
+         "SPEECHTITLE":"Going mobile with React Native and WebRTC",
+         "SPEECHABSTRACT":"Providing a mobile app is crucial. How would one go about doing this, if they built a web applicaion using WebRTC? React Native is a very solid solution, come and see how we leveraged it at Jitsi to go mobile.",
+         "TEXTIMG":"assets/img/speaker/saul-corretge.jpeg",
+         "TEXTTITLE":"Saúl Ibarra Corretgé",
+         "COMPANY":"8x8, Inc.",
+         "DESC":"",
+         "TWITTER":"https://twitter.com/@saghul",
+         "POSITION":""
+      },
+      {
+         "NAME":"Flavio Goncalves",
+         "SPEECHTITLE":"OpenSIPS and RabbitMQ",
+         "SPEECHABSTRACT":"In this presentation you will learn how to integrate OpenSIPS with RabbitMQ. The AMPQ protocol is an important architectural component to implement web-hooks, pre-paid, event-driven application. We will present the basics of RabbitMQ server and module configuration and simple applications.",
+         "TEXTIMG":"http://i.imgur.com/5PS7U7r.png",
+         "TEXTTITLE":"Flavio Goncalves",
+         "COMPANY":"SipPulse",
+         "DESC":"",
+         "TWITTER":"https://twitter.com/@flagonc",
+         "POSITION":"CTO"
       },
        {
          "NAME":"Bruno Haas",
          "SPEECHTITLE":"Interaction recording for CSPs, Call Centers and the Enterprise",
          "SPEECHABSTRACT":"This talk will take you through various aspects and techniques for recording interactions and will touch on the following subjects: SIPREC standard and OrecX sponsored OpenSIPS implementation, Packet interception, freeswitch mod_oreka, Voice Analytics, Screen recording, Legal aspects",
-         "TEXTIMG":"https://opensips.org/events/index.html/assets/img/logo.png",
+         "TEXTIMG":"assets/img/speaker/no-img.jpg",
          "TEXTTITLE":"Bruno Haas",
          "COMPANY":"OrecX LLC",
          "DESC":"Prior to co-founding OrecX, Bruno successively worked as a software architect in electronic warfare, high capacity optical switches and advanced speech processing systems at Thales, Nortel and Autonomy Systems.",
          "TWITTER":"",
          "POSITION":"Co-founder"
+      },
+      {
+         "NAME":"David Duffett",
+         "SPEECHTITLE":"Why Nerds Aren't Heard: How do Geeks sabotage their own presentations - and how to avoid these pitfalls",
+         "SPEECHABSTRACT":"",
+         "TEXTIMG":"https://secure.gravatar.com/avatar/c104436f064ea8d6eeb3038bf0127c1a?s=500",
+         "TEXTTITLE":"",
+         "COMPANY":"TeleSpeak",
+         "DESC":"Asterisk is the most popular open source IP telephony engine in the world today. With the addition of adaptors (that come in a range of shapes and sizes) Asterisk can be connected to most legacy PSTN interfaces like PRI, BRI, and FXO connections in addition to IP telephony protocols SIP and IAX2, which are natively supported.",
+         "TWITTER":"https://twitter.com/@dduffett",
+         "POSITION":""
       },
       {
          "NAME":"Razvan Crainea",
@@ -33,6 +55,17 @@ var summitData = {
          "TWITTER":"https://twitter.com/@razvancrainea",
          "POSITION":""
       },
+      {
+         "NAME":"Marvi Benedet",
+         "SPEECHTITLE":"VOIP solutions for the Public Amministration of the north-east Italy",
+         "SPEECHABSTRACT":"self made solution for 50+ districts using opensips/freeswitch/mysql/nginx/web2py. 3000+ clients using it now, fast growing. Model/View/Controller written in python for phone provisioning and all the configurations aspects Graphic rappresentation of the resulting configuration for every department",
+         "TEXTIMG":"assets/img/speaker/marvi-benedet.jpeg",
+         "TEXTTITLE":"Marvi Benedet",
+         "COMPANY":"Insiel S.p.A.",
+         "DESC":"46 years old, electronic eng. working in Insiel since 2002. Involved in VoIP in the last 4 years in a little but very efficent working group. Very interested in evolutions of telephony.",
+         "TWITTER":"twitter.com",
+         "POSITION":""
+      },
        {
          "NAME":"Liviu Chircu",
          "SPEECHTITLE":"",
@@ -41,8 +74,19 @@ var summitData = {
          "TEXTTITLE":"Liviu Chircu",
          "COMPANY":"OpenSIPS Solutions",
          "DESC":"",
-         "TWITTER":"twitter.com/liviuchircu",
+         "TWITTER":"twitter.com/@liviuchircu",
          "POSITION":""
+      },
+      {
+         "NAME":"Bogdan-Andrei Iancu",
+         "SPEECHTITLE":"Key Notes - OpenSIPS 3.0",
+         "SPEECHABSTRACT":"OpenISPS 3.0",
+         "TEXTIMG":"http://i.imgur.com/c0q8TjB.png",
+         "TEXTTITLE":"Bogdan-Andrei Iancu",
+         "COMPANY":"OpenSIPS Project",
+         "DESC":"Bogdan is the OpenSIPS project founder with an experience of 15+ year in the SIP world. Practicing the symbioses between managing the Open Source project and building commercial products around OpenSIPS, gives the best results in producing a viable SIP Server software for the real-life needs.",
+         "TWITTER":"https://twitter.com/@bogdan_iancu",
+         "POSITION":"Founder"
       },
       {
          "NAME":"Lorenzo Mangani",
@@ -52,22 +96,164 @@ var summitData = {
          "TEXTTITLE":"Lorenzo Mangani",
          "COMPANY":"QXIP",
          "DESC":"",
-         "TWITTER":"twitter.com/qxip",
+         "TWITTER":"https://twitter.com/@qxip",
          "POSITION":""
       },
       {
-         "NAME":"Marvi Benedet",
-         "SPEECHTITLE":"",
-         "SPEECHABSTRACT":"",
-         "TEXTIMG":"https://opensips.org/events/index.html/assets/img/logo.png",
-         "TEXTTITLE":"Marvi Benedet",
-         "COMPANY":"Insiel S.p.A.",
-         "DESC":"46 years old, electronic eng. working in Insiel since 2002. Involved in VoIP in the last 4 years in a little but very efficent working group. Very interested in evolutions of telephony.",
-         "TWITTER":"twitter.com",
+         "NAME":"Dan Christian Bogos",
+         "SPEECHTITLE":"Fraud mitigation using traffic pattern monitoring with CGRateS",
+         "SPEECHABSTRACT":"Will explore various mechanisms implemented in the new ThresholdS module of CGRateS, focusing not only on improving QoS stats but also towards securing networks with automatic reaction profiles against fraud attempts. CGRateS is an Enterprise Billing Suite with support for various billing modes.",
+         "TEXTIMG":"http://i.imgur.com/2D2AjWr.png",
+         "TEXTTITLE":"Dan Christian Bogos",
+         "COMPANY":"ITsysCOM GmbH",
+         "DESC":"He is the founder of ITsysCOM, experienced communications architect and VoIP specialist. Dan is a double graduate of Politechnica University, Timisoara, with post-graduate specialization in Communication Protocols and Software Development. A frequent and well-known contributor to the Open Source community, most noticeably being the co-founder of CGRateS Project, Dan is a firm believer in merging the very best production-ready software to create high-quality, scalable and cost-effective communications solutions.",
+         "TWITTER":"https://twitter.com/@danbogos",
+         "POSITION":"Founder"
+      },
+      {
+         "NAME":"Nitish Mahajan",
+         "SPEECHTITLE":"Telecommunication in India a survey and Predictive capacity estimation for VoIP.",
+         "SPEECHABSTRACT":"The talk covers three aspects: A. Current scenario and opportunities for telecommunication technologies in India B. Government initiatives and investments in the same C. Work being carried out at Telecommunications lab, Panjab University, Chandigarh, India(Software and Predictive Model Development)",
+         "TEXTIMG":"assets/img/speaker/no-img.jpg",
+         "TEXTTITLE":"Nitish Mahajan",
+         "COMPANY":"Panjab University, Chandigarh, India.",
+         "DESC":"",
+         "TWITTER":"https://twitter.com/",
          "POSITION":""
       },
-      
-    
+      {
+         "NAME":"Pete Kelly",
+         "SPEECHTITLE":"How Hihg Availability has evolved with OpenSIPS",
+         "SPEECHABSTRACT":"My talk proposes to cover how making OpenSIPS HA has evolved over the years and progressing through the versions of OpenSIPS (from vrrp+t_replicate, to dialog sharing with DB, to clustering and everyything in between). This will show how OpenSIPS has evolved with time and how easy it is to scale now",
+         "TEXTIMG":"https://user-images.githubusercontent.com/1423657/31589533-a7cf4f5e-b203-11e7-8a52-bf2f2cfdf72a.png",
+         "TEXTTITLE":"Pete Kelly",
+         "COMPANY":"SourceVox",
+         "DESC":"",
+         "TWITTER":"https://twitter.com/@p3k4y",
+         "POSITION":""
+      },
+      {
+         "NAME":"Maxim Sobolev",
+         "SPEECHTITLE":"Secure RTP support in the RTPproxy: WiP report",
+         "SPEECHABSTRACT":"When RTPproxy was designed in 2004 we never thought of it as being more than a low-overhead RTP forwarder to help with NAT traversal/media pinning tasks.  The idea was powerful enough to get a life of its own, so now we are exploring options to make it a usable tool for secure media interconnects.",
+         "TEXTIMG":"assets/img/speaker/maxim-sobolev.jpeg",
+         "TEXTTITLE":"Maxim Sobolev",
+         "COMPANY":"Sippy Software, Inc.",
+         "DESC":"Long term OpenSIPS contributor, open source aficionado. Author and maintainer of few open source projects.",
+         "TWITTER":"https://twitter.com/@sobomax",
+         "POSITION":""
+      },
+      {
+         "NAME":"Giovanni Maruzzelli",
+         "SPEECHTITLE":"OpenSIPS SIP-I, the ISUP, many Carriers, and FreeSWITCH",
+         "SPEECHABSTRACT":"How superb sip-i OpenSIPS module interact with powerful B2B FreeSWITCH capabilities to allow for Carrier-to-Carrier Services",
+         "TEXTIMG":"https://secure.gravatar.com/avatar/9a8d3098ee2abd638e99eae52de1d2b1?s=500",
+         "TEXTTITLE":"Giovanni Maruzzelli",
+         "COMPANY":"OpenTelecom.IT",
+         "DESC":"Giovanni Maruzzelli (OpenTelecom.IT) is heavily engaged with FreeSWITCH, of which he wrote the interface with Skype and with cellular phones. He's a consultant for the Telco sector, developing software and training courses for FreeSWITCH, SIP, WebRTC, Kamailio and OpenSIPS. An Internet tech pioneer, in 1996 Giovanni was cofounder of Italia Online, the most popular Italian portal and consumer ISP, and architect of its Internet technologies - www.italiaonline.it Then supervisor of Internet operations and architect of the first engine for paid access to www.ilsole24ore.com , the most read financial newspaper in Italy and to its databases (migrated from mainframe). After that, he was CEO of venture capital funded Matrice, developing Telemail unified messaging and multi language phone access to email (Text To Speech), and CTO of incubator funded Open4, an open source managed applications provider. Then he was for two years in Serbia as Internet and Telecommunication Investment Expert for World Bank - IFC. Since 2005 he's based in Italy, and serves ICT and Telco companies worldwide.",
+         "TWITTER":"https://twitter.com/@gmaruzz",
+         "POSITION":""
+      },
+      {
+         "NAME":"Lorenzo Miniero",
+         "SPEECHTITLE":"Put some Web in your RTC SIP Architecture with Janus",
+         "SPEECHABSTRACT":"A lot was invested on SIP infrastructures, they're still widely in use and they're there to stay: and while WebRTC provides easy access from the web, it shouldn't be a reason to throw them away. This talk explains how you can make pretty much any SIP-based solution WebRTC compliant with Janus.",
+         "TEXTIMG":"http://i.imgur.com/4rfCiZB.png",
+         "TEXTTITLE":"Lorenzo Miniero",
+         "COMPANY":"Meetecho",
+         "DESC":"Lorenzo Miniero is the chairman and co-founder of Meetecho, a company providing consulting services on everything related to real-time multimedia, while also regularly providing streaming and remote participation services for well known events around the world (e.g., IETF and ACM). Lorenzo received his degree and Ph.D. at the Computer Science Department of the University of Napoli Federico II, where he started working on multimedia conferencing and met the colleagues with whom he co-founded Meetecho as an academic spin-off. He is an active contributor to the Internet Engineering Task Force (IETF) standardization activities, especially in the framework of real-time multimedia applications. He is most known as the author of the Janus WebRTC Server, an open source WebRTC server-side implementation.",
+         "TWITTER":"https://twitter.com/@elminiero",
+         "POSITION":"Chairman"
+      },
+      {
+         "NAME":"John Quick",
+         "SPEECHTITLE":"Docker, Clustering, Scalability – a case study",
+         "SPEECHABSTRACT":"'SIP Registrar/Proxy in front of a FreeSwitch-based core; with WebRTC support; it must be Docker-based so we can use Kubernetes to make it scalable'. That was the spec I was given. My presentation explains how the design evolved and highlights some interesting snags found during development.",
+         "TEXTIMG":"assets/img/speaker/john-quick.jpeg",
+         "TEXTTITLE":"John Quick",
+         "COMPANY":"Smartvox Limited",
+         "DESC":"A lifelong programmer; working in telecoms for more than 25 years; working with OpenSIPS more than 12 years. I set up my own consulting company, based in the UK, back in 2004 and have worked with clients in many parts of the world.",
+         "TWITTER":"https://twitter.com/@Astragnome",
+         "POSITION":""
+      },
+      {
+         "NAME":"Oleg Agafonov",
+         "SPEECHTITLE":"VoIP Troubleshooting and Monitoring with SIP3",
+         "SPEECHABSTRACT":"Twenty Thousand MPS under the SIP: VoIP network troubleshooting and monitoring simplified",
+         "TEXTIMG":"assets/img/speaker/oleg-agafonov.jpeg",
+         "TEXTTITLE":"Oleg Agafonov",
+         "COMPANY":"SIP3.IO",
+         "DESC":"CTO and Co-founder at [SIP3](https://sip3.io) - RTC and VoIP traffic monitoring & analysis",
+         "TWITTER":"https://twitter.com/@agaf0x",
+         "POSITION":"CTO"
+      },
+      {
+         "NAME":"Ruben Homs",
+         "SPEECHTITLE":"Monitoring OpenSIPS with Prometheus",
+         "SPEECHABSTRACT":"Monitoring your infrastructure is an important part of keeping everything running. OpenSIPS exposes awesome statistics that can be used for this purpose. Using Prometheus, Grafana and the 'opensips_exporter' you'll be able to make graphs, alerts and debug the toughest issues.",
+         "TEXTIMG":"https://avatars2.githubusercontent.com/u/546961",
+         "TEXTTITLE":"Ruben Homs",
+         "COMPANY":"VoIPGRID",
+         "DESC":"",
+         "TWITTER":"https://twitter.com/",
+         "POSITION":""
+      },
+      {
+         "NAME":"Vasilios Tzanoudakis",
+         "SPEECHTITLE":"Geo-Redundant ITSP architecture using OpenSIPS 2.4",
+         "SPEECHABSTRACT":"I will present the design of our Global SIP architecture that we are currently developing as an ITSP. I will present all pieces needed when you are planning to build a Global SIP Infrastructure and why we choose OpenSIPS 2.4 to overcome almost all of our challenges.",
+         "TEXTIMG":"assets/img/speaker/Vasilios.Tzanoudakis.jpg",
+         "TEXTTITLE":"Vasilios Tzanoudakis",
+         "COMPANY":"VOICELAND P.C.",
+         "DESC":"",
+         "TWITTER":"https://twitter.com/@vtzan",
+         "POSITION":"Technology Director & Co-Founder"
+      },
+      {
+         "NAME":"Ameed Jamous",
+         "SPEECHTITLE":"How TelecomsXChange was built using Open-Source Software ?",
+         "SPEECHABSTRACT":"TelecomXChange is the first open wholesale telecom trading & interconnect platform built using open-source software. The platform allows you to interconnect via SIP to hundreds of carriers in minutes, no contracts, no commissions.",
+         "TEXTIMG":"assets/img/speaker/ameed-jamous.jpeg",
+         "TEXTTITLE":"Ameed Jamous",
+         "COMPANY":"TelecomsXChange",
+         "DESC":"Telecom Entrepreneur, Founder and CTO @ Telecomsxchange Platform. Partner and Tech Evangelist @TeleStax The Restcomm Company.",
+         "TWITTER":"https://twitter.com/@ameedjamous",
+         "POSITION":"CTO"
+      },
+      {
+         "NAME":"Celeste Mangani",
+         "SPEECHTITLE":"Business and OpenSource",
+         "SPEECHABSTRACT":"",
+         "TEXTIMG":"assets/img/speaker/celeste-mangani.jpeg",
+         "TEXTTITLE":"Celeste Mangani",
+         "COMPANY":"QXIP",
+         "DESC":"",
+         "TWITTER":"https://twitter.com/@lightbluen",
+         "POSITION":"Program Manager"
+      },
+      {
+         "NAME":"Nitzan Gutman",
+         "SPEECHTITLE":"",
+         "SPEECHABSTRACT":"",
+         "TEXTIMG":"assets/img/speaker/Nitzan_Gutman.jpg",
+         "TEXTTITLE":"Nitzan Gutman",
+         "COMPANY":"Voicenter Ltd",
+         "DESC":"",
+         "TWITTER":"https://twitter.com/@VoicenterCloud",
+         "POSITION":"CEO"
+      },
+      {
+         "NAME":"Michael Mavroudis",
+         "SPEECHTITLE":"",
+         "SPEECHABSTRACT":"",
+         "TEXTIMG":"assets/img/speaker/Michael_Mavroudis.jpg",
+         "TEXTTITLE":"Michael Mavroudis",
+         "COMPANY":"FreeSWITCH",
+         "DESC":"Michael Mavroudis is the Community Liaison for the FreeSWITCH project as well as the owner of VoiceTel a VoIP provider committed to the delivery of superior quality, highly resilient, and fault tolerant IP-based telecommunications services.",
+         "TWITTER":"",
+         "POSITION":""
+      },
+
    ],
    
    "schedule": [ 
